57e78c1224090a872a044c513686d6461ad6a86f
Jovi <jvay21@gmail.com>
2024-04-24 22:35:22 -0500

    feat: dates

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
   endDate = new Date('2024-04-26T20:00:00');
+
+  hideRain(): boolean {
+    const now = new Date();
+    const isToday =
+      now.getDate() === this.endDate.getDate() &&
+      now.getMonth() === this.endDate.getMonth() &&
+      now.getFullYear() === this.endDate.getFullYear() &&
+      now.getHours() === this.endDate.getHours() &&
+      now.getMinutes() === this.endDate.getMinutes() &&
+      now.getSeconds() === this.endDate.getSeconds();
+    return isToday;
+  }
 }
